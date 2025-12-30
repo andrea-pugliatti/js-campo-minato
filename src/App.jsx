4928802375366eab9a5b3dc43870b45ba31d210f
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-30 14:38:42 +0100

    Add basic grid to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,80 @@
+/** biome-ignore-all lint/correctness/useExhaustiveDependencies: <Biome bug> */
+import { useEffect, useState } from "react";
+
 function App() {
+	const [grid, setGrid] = useState([]);
+	const [bombs, setBombs] = useState([]);
+	const [gameOver, setGameOver] = useState(false);
+
+	const emptyCell = {
+		isActive: false,
+		closeMines: 0,
+	};
+
+	function initializeGrid() {
+		const newGrid = [];
+
+		for (let i = 0; i < 100; i++) {
+			newGrid.push(emptyCell);
+		}
+
+		setGrid(newGrid);
+	}
+
+	const getRandomNumber = (min, max) =>
+		Math.floor(Math.random() * (max - min + 1)) + min;
+
+	const checkBomb = (index) => bombs.includes(index);
+
+	// const updateCells = (index) => {
+	//   const newGrid = [...grid];
+	//   if (index >= 10) {
+
+	//   }
+	//   newGrid[index]
+	// };
+
+	function initializeBombs() {
+		const array = [];
+
+		for (let i = 0; i < 16; i++) {
+			const randomNum = getRandomNumber(0, 99);
+			array.push(randomNum);
+			updateCells(randomNum);
+		}
+
+		setBombs(array);
+	}
+
+	useEffect(() => {
+		initializeGrid();
+		initializeBombs();
+	}, []);
+
 	return (
-		<>
-			<h1>Henlo</h1>
-		</>
+		<div className="container">
+			<div className="grid">
+				{grid.map((current, index) => (
+					<button
+						type="button"
+						key={`cell-${current.isActive}-${index}`}
+						className={`cell ${current.isActive ? "active" : ""}`}
+						onClick={() => {
+							if (checkBomb(index)) {
+								console.log("Exploded", index, bombs);
+								setGameOver(true);
+							}
+
+							const newGrid = [...grid];
+							newGrid[index] = { ...newGrid[index], isActive: true };
+							setGrid(newGrid);
+						}}
+					>
+						{index + 1}
+					</button>
+				))}
+			</div>
+		</div>
 	);
 }
 
