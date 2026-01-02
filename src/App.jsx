943f1097edaf687a5d7ac57dc37ff6295328e8d9
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-01-02 15:25:11 +0100

    Add difficulties

--- src/App.jsx
+++ src/App.jsx
@@ -1,283 +1,52 @@
 /** biome-ignore-all lint/correctness/useExhaustiveDependencies: <Biome bug> */
 /** biome-ignore-all lint/suspicious/noArrayIndexKey: <Ignore for now> */
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import LostFace from "./assets/img/faces/lostface.svg";
-import SmileFace from "./assets/img/faces/smileface.svg";
-
-import CellDown from "./assets/img/cells/celldown.svg";
-import CellFlag from "./assets/img/cells/cellflag.svg";
-import CellMine from "./assets/img/cells/cellmine.svg";
-import CellUp from "./assets/img/cells/cellup.svg";
-
-import Cell1 from "./assets/img/cells/cell1.svg";
-import Cell2 from "./assets/img/cells/cell2.svg";
-import Cell3 from "./assets/img/cells/cell3.svg";
-import Cell4 from "./assets/img/cells/cell4.svg";
-import Cell5 from "./assets/img/cells/cell5.svg";
-import Cell6 from "./assets/img/cells/cell6.svg";
-import Cell7 from "./assets/img/cells/cell7.svg";
-import Cell8 from "./assets/img/cells/cell8.svg";
-
-import Counter0 from "./assets/img/counter/counter0.svg";
-import Counter1 from "./assets/img/counter/counter1.svg";
-import Counter2 from "./assets/img/counter/counter2.svg";
-import Counter3 from "./assets/img/counter/counter3.svg";
-import Counter4 from "./assets/img/counter/counter4.svg";
-import Counter5 from "./assets/img/counter/counter5.svg";
-import Counter6 from "./assets/img/counter/counter6.svg";
-import Counter7 from "./assets/img/counter/counter7.svg";
-import Counter8 from "./assets/img/counter/counter8.svg";
-import Counter9 from "./assets/img/counter/counter9.svg";
-import CounterDash from "./assets/img/counter/counter-.svg";
+import CampoMinato from "./components/CampoMinato";
 
 function App() {
-	const width = 10;
-	const height = 10;
-	const nBombs = 10;
+	const [choice, setChoice] = useState(null);
 
-	const [grid, setGrid] = useState([]);
-	const [bombs, setBombs] = useState([]);
-	const [mineCounters, setMineCounters] = useState([]);
-	const [flagged, setFlagged] = useState([]);
-	const [gameOver, setGameOver] = useState(false);
-
-	function initializeGame() {
-		const newGrid = [];
-		const newCounters = [];
-		const bombs = [];
-
-		for (let i = 0; i < width * height; i++) {
-			newGrid.push(false);
-			newCounters.push(0);
-		}
-
-		while (bombs.length < nBombs) {
-			const randomNum = getRandomNumber(0, 99);
-			if (!bombs.includes(randomNum)) bombs.push(randomNum);
-		}
-
-		setMineCounters(newCounters);
-		setGrid(newGrid);
-		setBombs(bombs);
-		setFlagged([]);
-		setGameOver(false);
-	}
-
-	const getRandomNumber = (min, max) =>
-		Math.floor(Math.random() * (max - min + 1)) + min;
-
-	const checkBomb = (index) => bombs.includes(index);
-
-	function updateCells() {
-		const newCounters = Array(width * height).fill(0);
-
-		for (const bomb of bombs) {
-			const row = Math.floor(bomb / width);
-			const col = bomb % width;
-
-			for (let r = row - 1; r <= row + 1; r++) {
-				for (let c = col - 1; c <= col + 1; c++) {
-					const index = r * width + c;
-
-					// If out of bounds
-					if (r < 0 || r >= height || c < 0 || c >= width) continue;
-					// If it's the bomb itself
-					if (index === bomb || bombs.includes(index)) continue;
-
-					newCounters[index] += 1;
-				}
-			}
-		}
-
-		setMineCounters(newCounters);
-	}
-
-	function revealCells(row, col, grid) {
-		const index = row * width + col;
-
-		// If out of bounds
-		if (row < 0 || row >= height || col < 0 || col >= width) return;
-		// If already active
-		if (grid[index]) return;
-
-		// Reveal the current cell
-		grid[index] = true;
-
-		// If this cell has neighboring mines, stop
-		if (mineCounters[index] > 0) return;
-
-		// If it's a zero cell, visit all 8 neighbors
-		for (let offX = -1; offX <= 1; offX++) {
-			for (let offY = -1; offY <= 1; offY++) {
-				// Skip the current cell itself
-				if (offX === 0 && offY === 0) continue;
-
-				revealCells(row + offX, col + offY, grid);
-			}
-		}
-	}
-
-	function showCell(current, index) {
-		if (bombs.includes(index) && gameOver) {
-			return CellMine;
-		}
-
-		if (current) {
-			switch (mineCounters[index]) {
-				case 1:
-					return Cell1;
-				case 2:
-					return Cell2;
-				case 3:
-					return Cell3;
-				case 4:
-					return Cell4;
-				case 5:
-					return Cell5;
-				case 6:
-					return Cell6;
-				case 7:
-					return Cell7;
-				case 8:
-					return Cell8;
-				default:
-					return CellDown;
-			}
-		}
-
-		if (flagged.includes(index)) {
-			return CellFlag;
-		}
-
-		return CellUp;
-	}
-
-	function showCounter(number) {
-		const string = number.toString().trim();
-		const array = [];
-
-		for (let i = 0; i <= string.length; i++) {
-			switch (string.charAt(i)) {
-				case "0":
-					array.push(Counter0);
-					break;
-				case "1":
-					array.push(Counter1);
-					break;
-				case "2":
-					array.push(Counter2);
-					break;
-				case "3":
-					array.push(Counter3);
-					break;
-				case "4":
-					array.push(Counter4);
-					break;
-				case "5":
-					array.push(Counter5);
-					break;
-				case "6":
-					array.push(Counter6);
-					break;
-				case "7":
-					array.push(Counter7);
-					break;
-				case "8":
-					array.push(Counter8);
-					break;
-				case "9":
-					array.push(Counter9);
-					break;
-				case "-":
-					array.push(CounterDash);
-					break;
-			}
-		}
-
-		while (array.length < 3) {
-			array.unshift(Counter0);
-		}
-
-		return array;
-	}
-
-	useEffect(() => {
-		initializeGame();
-	}, []);
-
-	useEffect(() => {
-		updateCells();
-	}, [bombs]);
+	const difficulties = [
+		{ width: 9, height: 9, nBombs: 10 },
+		{ width: 16, height: 16, nBombs: 40 },
+		{ width: 30, height: 16, nBombs: 99 },
+	];
 
 	return (
 		<div className="container">
-			<div className="minesweeper">
-				<div className="minesweeper-top">
-					<div className="bombs-number">
-						{showCounter(nBombs - flagged.length).map((current, index) => {
-							return (
-								<img
-									key={`counter-${index}`}
-									height={40}
-									src={current}
-									alt=""
-								/>
-							);
-						})}
-					</div>
-					<div className="face">
-						<button type="button" onClick={initializeGame}>
-							{gameOver ? (
-								<img height={40} src={LostFace} alt="smiley" />
-							) : (
-								<img height={40} src={SmileFace} alt="smiley" />
-							)}
-						</button>
-					</div>
-					<div className="time">
-						<img height={40} src={Counter0} alt="" />
-						<img height={40} src={Counter0} alt="" />
-						<img height={40} src={Counter0} alt="" />
-					</div>
+			<h1>Campo Minato</h1>
+			{choice === null ? (
+				<div className="menu">
+					<button
+						type="button"
+						className="choice-button"
+						onClick={() => setChoice(0)}
+					>
+						Beginner
+					</button>
+					<button
+						type="button"
+						className="choice-button"
+						onClick={() => setChoice(1)}
+					>
+						Intermediate
+					</button>
+					<button
+						type="button"
+						className="choice-button"
+						onClick={() => setChoice(2)}
+					>
+						Expert
+					</button>
 				</div>
-				<div className="grid">
-					{grid.map((current, index) => (
-						<button
-							type="button"
-							key={`cell-${index}`}
-							disabled={gameOver}
-							className={`cell ${current ? "active" : ""}`}
-							onClick={() => {
-								if (checkBomb(index)) {
-									console.log("Exploded", index, bombs);
-									setGameOver(true);
-								}
-
-								const row = Math.floor(index / width);
-								const col = index % width;
-
-								const newGrid = [...grid];
-								revealCells(row, col, newGrid);
-								setGrid(newGrid);
-							}}
-							onContextMenu={(e) => {
-								e.preventDefault();
-
-								if (flagged.includes(index)) {
-									const newArray = flagged.filter((item) => item === index);
-									setFlagged(newArray);
-								} else {
-									const newArray = [...flagged, index];
-									setFlagged(newArray);
-								}
-							}}
-						>
-							{<img height={30} src={showCell(current, index)} alt="" />}
-						</button>
-					))}
-				</div>
-			</div>
+			) : (
+				<CampoMinato
+					width={difficulties[choice].width}
+					height={difficulties[choice].height}
+					nBombs={difficulties[choice].nBombs}
+				/>
+			)}
 		</div>
 	);
 }
